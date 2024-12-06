b71f95bf4daf681285d62bb5fca6384e92686285
Akash Desarda <desardaakash@gmail.com>
2024-12-06 06:39:06 +0000

    feat: :sparkles: Added Yahoo Finance aware ticker and exchange check dependencies

--- investing/api/dependency/utils.py
+++ investing/api/dependency/utils.py
@@ -1,11 +1,12 @@
-from collections import namedtuple
 from typing import Annotated
 
-from fastapi import Path
+from fastapi import Body, HTTPException, Path, status
 
-from investing.core.models import StockExchange, StockExchangeYahooIdentifier
-
-TickerIdentifier = namedtuple("TickerIdentifier", ["symbol", "exch_id"])
+from investing.core.models import (
+    StockExchange,
+    StockExchangeYahooIdentifier,
+    YahooTickerIdentifier,
+)
 
 
 async def yahoo_finance_aware_ticker(
@@ -16,7 +17,32 @@ async def yahoo_finance_aware_ticker(
         ),
     ],
     ticker: Annotated[str, Path(description="Desired company's `Ticker` symbol")],
-) -> TickerIdentifier:
+) -> YahooTickerIdentifier:
+    """Dependency to get Yahoo Finance aware ticker symbol"""
     ticker = ticker.upper()  # making sure that ticker symbol are always Upper case
-    yahoo_exch_id = getattr(StockExchangeYahooIdentifier, exchange.name)
-    return TickerIdentifier(ticker, yahoo_exch_id.value)
+    return YahooTickerIdentifier(
+        symbol=ticker,
+        exchange=exchange.name.upper(),
+        exch_id=getattr(StockExchangeYahooIdentifier, exchange.name),
+    )
+
+
+async def yahoo_finance_aware_exchange_check(
+    exchange: Annotated[
+        list,
+        Body(
+            embed=True,
+            description="List of desired exchanges of which tickers are needed to be fetched",
+            examples=[["NSE", "BSE", "NYSE"], ["TSO", "LSO", "BSE"]],
+        ),
+    ],
+):
+    """Dependency to get Yahoo Finance aware exchange"""
+    exchange_name = {exch.name for exch in StockExchange}
+    diff = list(set(exchange).difference(exchange_name))
+    if diff:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"The following exchanges are not supported: {diff}",
+        )
+    return exchange
