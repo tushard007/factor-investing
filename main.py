cf910bdf4ab6e52ea42f0741484a4c119bc3e475
Akash Desarda <desardaakash@gmail.com>
2024-12-06 07:17:39 +0000

    chore: :bookmark: Bump version to 0.3.1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from investing.api.routers import bulk, per_security
 from investing.core.models import APITags
 
-app = FastAPI(title="Factor Investing API", version="0.1")
+app = FastAPI(title="Factor Investing API", version="0.3.1")
 
 
 # Health Check
