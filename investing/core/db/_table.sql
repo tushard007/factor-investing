af3e954b042404898c38b895883e30821a9479b3
Akash Desarda <desardaakash@gmail.com>
2024-12-10 06:55:35 +0000

    added index

--- investing/core/db/_table.sql
+++ investing/core/db/_table.sql
@@ -2,11 +2,14 @@ create table if not exists factor_investing.ticker_history
 (
     date         date,
     ticker       text,
-    key          text,
+    key          text primary key,
     open         double precision,
     high         double precision,
     low          double precision,
     close        double precision,
     volume       double precision,
     stock_splits double precision
-)
+);
+
+create index idx_key
+    on factor_investing.ticker_history (key);
