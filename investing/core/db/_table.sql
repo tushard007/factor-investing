eab4dc7e2a04d5250e7ea6e93f2aaad177658bfe
Akash Desarda <desardaakash@gmail.com>
2024-12-13 17:11:36 +0000

    refactor: :wastebasket: remove not required columns from ticker_history table and SQL schema

--- investing/core/db/_table.sql
+++ investing/core/db/_table.sql
@@ -1,14 +1,12 @@
 create table if not exists factor_investing.ticker_history
 (
-    date         date,
-    ticker       text,
-    key          text primary key,
-    open         double precision,
-    high         double precision,
-    low          double precision,
-    close        double precision,
-    volume       double precision,
-    stock_splits double precision
+    date   date,
+    ticker text,
+    key    text primary key,
+    open   double precision,
+    high   double precision,
+    low    double precision,
+    close  double precision
 );
 
 create index idx_key
