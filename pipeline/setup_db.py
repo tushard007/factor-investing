1602bec60f167711a2a1a06ca347658b4e3c35f2
Akash Desarda <desardaakash@gmail.com>
2024-12-14 20:22:27 +0530

    🐛 bug fix: fixed the database setup script

--- pipeline/setup_db.py
+++ pipeline/setup_db.py
@@ -1,25 +1,41 @@
 import logging
+from pathlib import Path
 
-import psycopg
 from dotenv import dotenv_values
+from psycopg import connect
 
 from investing.core.db import schema_ddl, table_ddl
 
 logger = logging.getLogger("factor-investing")
+current_path = Path(__file__).resolve()
+config = dotenv_values(current_path.parent.parent / ".env")
 
-config = dotenv_values("../.env")
-
-conn = psycopg.connect(
-    f"postgresql://{config['USER']}:{config['PASSWORD']}@localhost:5432/playground"
+conn = connect(
+    f"postgresql://{config['USER']}:{config['PASSWORD']}@localhost:5432/postgres",
 )
-with conn.cursor() as cursor:
-    # cursor.execute(database_ddl)
-    logger.info(cursor.execute("select * from test.dummy1 limit 1").fetchone())
-    logger.info("Database setup complete")
-    logger.info(schema_ddl)
-    cursor.execute(schema_ddl)
-    logger.info("Schema setup complete")
-    cursor.execute(table_ddl)
-    logger.info("Table setup complete")
-    conn.commit()
+conn.autocommit = True
+cursor = conn.cursor()
+
+cursor.execute("SELECT 1 FROM pg_database WHERE datname = 'playground'")
+if exists := cursor.fetchone():
+    logger.info("Database 'playground' already exists")
+
+else:
+    cursor.execute("CREATE DATABASE playground")
+    logger.info("Database 'playground' created")
+cursor.close()
 conn.close()
+
+# Connect to the newly created database
+conn = connect(
+    f"postgresql://{config['USER']}:{config['PASSWORD']}@localhost:5432/playground",
+)
+cursor = conn.cursor()
+
+cursor.execute(schema_ddl)
+logger.info("Schema setup complete")
+conn.commit()
+
+cursor.execute(table_ddl)
+logger.info("Table setup complete")
+conn.commit()
