2326a0ff805f2247d21d2462dfd45102d7e65005
Akash Desarda <desardaakash@gmail.com>
2024-12-18 10:22:04 +0530

    ✨ chore: simplify log format initialization

--- investing/__init__.py
+++ investing/__init__.py
@@ -12,9 +12,7 @@ console_handler = RichHandler(
     console=Console(),
     locals_max_string=150,
 )
-log_format = logging.Formatter(
-    "%(asctime)s\t%(message)s", datefmt="%d-%b-%y %H:%M:%S"
-)
+log_format = logging.Formatter("%(asctime)s\t%(message)s", datefmt="%d-%b-%y %H:%M:%S")
 console_handler.setFormatter(log_format)
 logger.addHandler(console_handler)
 logger.setLevel(logging.DEBUG)
